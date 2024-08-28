e0abdcf6979f824bba35d5c67bb05c13ee7a473d
Nikita Tsutskov <tsutskovn@gmail.com>
2024-08-29 01:48:02 +0300

    Version with mistake in sum in last 3 element

--- main.cpp
+++ main.cpp
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define SIZE_Y 4
+#define SIZE_Y 5
 #define SIZE_X 4
 
 // Функция для вывода массива
 static void PrintArray(int array_a[SIZE_Y][SIZE_X])
 {
     printf("Массив:\n");
-    for (int i = 0; i < SIZE; i++)
+    for (int i = 0; i < SIZE_Y; i++)
     {
-        for (int j = 0; j < SIZE; j++)
+        for (int j = 0; j < SIZE_X; j++)
         {
             printf("%d ", array_a[i][j]);
         }
@@ -20,58 +20,77 @@ static void PrintArray(int array_a[SIZE_Y][SIZE_X])
 }
 
 // Функция для транспонирования массива
-static void TransposeArray(int array_a[SIZE_Y][SIZE_X], int res[SIZE][SIZE])
+static int TransposeArray(int array_a[SIZE_Y][SIZE_X], int res[SIZE_Y][SIZE_X])
 {
-    for (int i = 0; i < SIZE; i++)
+    if (SIZE_Y != SIZE_X)
     {
-        for (int j = 0; j < SIZE; j++)
+        printf ("\nImposible operation\n");
+        return 0;
+    }
+    else
+    {
+        for (int i = 0; i < SIZE_Y; i++)
         {
-            res[j][i] = array_a[i][j];
+            for (int j = 0; j < SIZE_X; j++)
+            {
+                res[j][i] = array_a[i][j];
+            }
         }
     }
+    return 1;
 }
 
 static void FillArray(int array_a[SIZE_Y][SIZE_X])
 {
 // Заполнение массива случайными числами
-    for (int i = 0; i < SIZE; i++)
+    for (int i = 0; i < SIZE_Y; i++)
     {
-        for (int j = 0; j < SIZE; j++)
+        for (int j = 0; j < SIZE_X; j++)
         {
             array_a[i][j] = rand() % 100; // Генерация случайного числа от 0 до 99
         }
     }
 }
 
-static void SumOfMatrix(int array_a[SIZE_Y][SIZE_X], int array_b[SIZE][SIZE], int array_res[SIZE][SIZE])
+static void SumOfMatrix(int array_a[SIZE_Y][SIZE_X], int array_b[SIZE_Y][SIZE_X], int res[SIZE_Y][SIZE_X])
 {
     for (int i = 0; i < SIZE_Y; i++)
     {
         for (int j = 0; j < SIZE_X; j++)
         {
-            transposed[j][i] = array_a[i][j];
+            res[j][i] = array_a[j][i] + array_b[j][i];
         }
     }
 }
 
 int main()
 {
-    int array_a[SIZE_Y][SIZE_X];
-    int array_b[a][SIZE_X];
-    int res[SIZE][SIZE];
+    int array_a[SIZE_Y][SIZE_X] = {};
+    int array_b[SIZE_Y][SIZE_X] = {};
+    int res[SIZE_Y][SIZE_X] = {};
 
     // Инициализация генератора случайных чисел
     srand(time(NULL));
 
     FillArray(array_a);
+    FillArray(array_b);
+    FillArray(res);
 
-    printf("\nИсходный массив:\n");
+    printf("\nИсходный массив a:\n");
     PrintArray(array_a);
 
-    TransposeArray(array_a, transposed);
+    printf("\nИсходный массив b:\n");
+    PrintArray(array_b);
+
+    TransposeArray(array_a, res);
 
     printf("\nТранспонированный массив:\n");
-    PrintArray(transposed);
+    PrintArray(res);
+
+    SumOfMatrix(array_a, array_b, res);
+
+    printf("\nСумма массивов:\n");              //!!!!!!
+    PrintArray(res);
 
     return 0;
 }
