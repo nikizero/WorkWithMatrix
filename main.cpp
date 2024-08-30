184643783b337b4339baf040fb8f3d5daaaa1222
Nikita Tsutskov <tsutskovn@gmail.com>
2024-08-30 15:52:30 +0300

    Реализация через динамику

--- main.cpp
+++ main.cpp
@@ -1,97 +1,108 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <malloc.h>
-                                                                    
-// Функция для вывода массива     
 
-static void PrintArray(int array_a[SIZE_Y][SIZE_X])                                     
-{
-    printf("Массив:\n");
-    for (int i = 0; i < SIZE_Y; i++)
-    {
-        for (int j = 0; j < SIZE_X; j++)
-        {
-            printf("%d ", array_a[i][j]);
+// Функция для генерации случайного числа в диапазоне от 0 до 9
+int random_number() {
+    return rand() % 10;
+}
+
+// Функция для создания и заполнения матрицы случайными числами
+int** create_matrix(int rows, int cols) {
+    int** matrix = (int**)malloc(rows * sizeof(int*));
+    for (int i = 0; i < rows; i++) {
+        matrix[i] = (int*)malloc(cols * sizeof(int));
+        for (int j = 0; j < cols; j++) {
+            matrix[i][j] = random_number();
+        }
+    }
+    return matrix;
+}
+
+// Функция для вывода матрицы
+void print_matrix(int** matrix, int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
         }
         printf("\n");
     }
 }
 
-// Функция для транспонирования массива
-static int TransposeArray(int array_a[SIZE_Y][SIZE_X], int res[SIZE_Y][SIZE_X])
-{
-    if (SIZE_Y != SIZE_X)
-    {
-        printf ("\nImposible operation\n");
-        return 1;
-    }
-    else
-    {
-        for (int i = 0; i < SIZE_Y; i++)
-        {
-            for (int j = 0; j < SIZE_X; j++)
-            {
-                res[j][i] = array_a[i][j];
-            }
+// Функция для сложения двух матриц
+int** add_matrices(int** matrix1, int** matrix2, int rows, int cols) {
+    int** result = (int**)malloc(rows * sizeof(int*));
+    for (int i = 0; i < rows; i++) {
+        result[i] = (int*)malloc(cols * sizeof(int));
+        for (int j = 0; j < cols; j++) {
+            result[i][j] = matrix1[i][j] + matrix2[i][j];
         }
     }
-    return 0;
+    return result;
 }
 
-static void FillArray(int array_a[SIZE_Y][SIZE_X])
-{
-// Заполнение массива случайными числами
-    for (int i = 0; i < SIZE_Y; i++)
-    {
-        for (int j = 0; j < SIZE_X; j++)
-        {
-            array_a[i][j] = rand() % 100; // Генерация случайного числа от 0 до 99
+// Функция для транспонирования матрицы
+int** transpose_matrix(int** matrix, int size) {
+    int** transposed = (int**)malloc(size * sizeof(int*));
+    for (int i = 0; i < size; i++) {
+        transposed[i] = (int*)malloc(size * sizeof(int));
+        for (int j = 0; j < size; j++) {
+            transposed[i][j] = matrix[j][i];
         }
     }
+    return transposed;
 }
 
-static void SumOfMatrix(int array_a[SIZE_Y][SIZE_X], int array_b[SIZE_Y][SIZE_X], int res[SIZE_Y][SIZE_X])
-{
-    for (int i = 0; i < SIZE_Y; i++)
-    {
-        for (int j = 0; j < SIZE_X; j++)
-        {
-            res[i][j] = array_a[i][j] + array_b[i][j];
-        }
+// Функция для освобождения памяти
+void free_matrix(int** matrix, int rows) {
+    for (int i = 0; i < rows; i++) {
+        free(matrix[i]);
     }
+    free(matrix);
 }
 
-int main()
-{
-    int* size_xy = int* calloc(2, sizeof(int));
-    
-    int array_a[SIZE_Y][SIZE_X] = {};
-    int array_b[SIZE_Y][SIZE_X] = {};
-    int res[SIZE_Y][SIZE_X] = {};
+int main() {
+    int rows, cols;
 
     // Инициализация генератора случайных чисел
     srand(time(NULL));
 
-    FillArray(array_a);
-    FillArray(array_b);
-    FillArray(res);
+    // Ввод размеров матриц
+    printf("Введите количество строк: ");
+    scanf("%d", &rows);
+    printf("Введите количество столбцов: ");
+    scanf("%d", &cols);
 
-    printf("\nИсходный массив a:\n");
-    PrintArray(array_a);
+    // Создание и заполнение матриц
+    int** matrix1 = create_matrix(rows, cols);
+    int** matrix2 = create_matrix(rows, cols);
 
-    printf("\nИсходный массив b:\n");
-    PrintArray(array_b);
+    // Вывод матриц
+    printf("Матрица 1:\n");
+    print_matrix(matrix1, rows, cols);
 
-    TransposeArray(array_a, res);
+    printf("Матрица 2:\n");
+    print_matrix(matrix2, rows, cols);
 
-    printf("\nТранспонированный массив:\n");
-    PrintArray(res);
+    // Сложение матриц
+    int** sum_matrix = add_matrices(matrix1, matrix2, rows, cols);
+    printf("Результат сложения матриц:\n");
+    print_matrix(sum_matrix, rows, cols);
 
-    SumOfMatrix(array_a, array_b, res);
+    // Транспонирование, если матрица квадратная
+    if (rows == cols) {
+        int** transposed_matrix = transpose_matrix(matrix1, rows);
+        printf("Транспонированная Матрица 1:\n");
+        print_matrix(transposed_matrix, rows, cols);
+        free_matrix(transposed_matrix, rows);
+    } else {
+        printf("Матрица не является квадратной, транспонирование невозможно.\n");
+    }
 
-    printf("\nСумма массивов:\n");        
-    PrintArray(res);
+    // Освобождение памяти
+    free_matrix(matrix1, rows);
+    free_matrix(matrix2, rows);
+    free_matrix(sum_matrix, rows);
 
     return 0;
 }
